38f8a467eb370ae7609b6e4cdaafa4ecf9fc699c
highjump0603 <yunchan0603@gmail.com>
2025-08-22 02:45:08 +0900

    ㅁㄴ

--- src/App.js
+++ src/App.js
@@ -15,15 +15,19 @@ import AddressManagement from './pages/AddressManagement';
 import SavedRestaurants from './pages/SavedRestaurants';
 import ProfileEdit from './pages/ProfileEdit';
 import Settings from './pages/Settings';
+import useScrollToTop from './hooks/useScrollToTop';
 import './Font.css';
 import './App.css';
 
-function App() {
+// 스크롤 초기화를 위한 내부 컴포넌트
+function AppContent() {
+  // 페이지 이동 시 스크롤을 맨 위로 초기화
+  useScrollToTop();
+  
   return (
-    <Router>
-      <div className="App">
-        <main className="main-content">
-                  <Routes>
+    <div className="App">
+      <main className="main-content">
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/plan" element={<Plan />} />
           <Route path="/explore" element={<Explore />} />
@@ -40,8 +44,15 @@ function App() {
           <Route path="/profile-edit" element={<ProfileEdit />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
-        </main>
-      </div>
+      </main>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
---
+++ src/hooks/useScrollToTop.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // 페이지 이동 시 스크롤을 맨 위로 이동
+    window.scrollTo(0, 0);
+  }, [pathname]);
+};
+
+export default useScrollToTop;
